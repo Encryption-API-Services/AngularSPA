9b7c69d665514446d1d087c50442d6540f18460e
Mike Mulchrone <mtmulch0191@outlook.com>
2022-10-19 15:42:14 -0400

    register value

--- src/app/modules/register/register/register.component.ts
+++ src/app/modules/register/register/register.component.ts
@@ -1,4 +1,7 @@
+// deno-lint-ignore-file no-inferrable-types
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { passwordValidator } from "../../../validators/form-validators";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public canSubmit: boolean = false;
+  public submitClicked: boolean = false;
+  public registerForm: FormGroup = this.formBuilder.group({
+    email: ['',
+           [Validators.required, Validators.email]
+    ],
+    username: ['', [Validators.required, Validators.minLength(6)]],
+    password: ['', [passwordValidator]]
+  });
 
+  constructor(private formBuilder: FormBuilder) {
+    
+   }
+
+  
   ngOnInit(): void {
+    
   }
-
+  
+  public handleFormSubmit($event: any) {
+    this.submitClicked = !this.submitClicked;
+    console.log(this.registerForm);
+    if (this.registerForm.valid) {
+      console.log("Valid");
+      this.canSubmit = true;
+    } else {
+      console.log("invalid");
+      this.submitClicked = false;
+    }
+  }
 }
