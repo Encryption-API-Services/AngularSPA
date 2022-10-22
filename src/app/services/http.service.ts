806e35a1a1254cb099b1017a3ba27a1b89bbe5dd
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-10-21 23:38:28 -0400

    Update http.service.ts
    adding other http methods to HttpService.

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -8,7 +8,19 @@ export class HttpService {
 
   constructor(private http: HttpClient) { }
 
-  public get(url: string): Observable<any> {
+  public get(url: string) {
     return this.http.get(url);
+  }
+
+  public post(url: string, body: object) {
+    return this.http.post(url, body);
+  }
+
+  public put(url: string, body: object) {
+    return this.http.put(url, body);
+  }
+
+  public delete(url: string, body: object) {
+    return this.http.delete(url, body);
   }
 }
