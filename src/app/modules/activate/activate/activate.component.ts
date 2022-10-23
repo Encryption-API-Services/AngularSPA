5a72fbae18ee20af11846c45a8a02580876d1f2d
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-10-23 18:05:17 -0400

    email authentication

--- src/app/modules/activate/activate/activate.component.ts
+++ src/app/modules/activate/activate/activate.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-activate',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: ActivatedRoute, private http: HttpService) { }
 
   ngOnInit(): void {
+    const body = {
+      "id": this.router.snapshot.queryParamMap.get('id')
+    }
+    this.http.put("/api/UserRegister/Activate/", body).subscribe(response => {
+      console.log(response);
+    }, (error) => {
+      console.error(error);
+    });
   }
-
 }
