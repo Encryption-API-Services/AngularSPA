9637d78e6ccc093e2af39a49129138736c188020
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-10-24 05:24:06 -0400

    auth guard for user home page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   { path: "register", loadChildren: () => import(`./modules/register/register.module`).then(module => module.RegisterModule) },
   { path: "login", loadChildren: () => import(`./modules/login/login.module`).then(module => module.LoginModule) },
   { path: "about", loadChildren: () => import(`./modules/about/about.module`).then(module => module.AboutModule) },
-  { path: "activate", loadChildren: () => import(`./modules/activate/activate.module`).then(module => module.ActivateModule)}
+  { path: "activate", loadChildren: () => import(`./modules/activate/activate.module`).then(module => module.ActivateModule) },
+  { path: "user-home", loadChildren: () => import(`./modules/user-home/user-home.module`).then(module => module.UserHomeModule), canActivate: [AuthGuardService] }
 ];
 
 @NgModule({
---
+++ src/app/services/auth-guard.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+
+  constructor() { }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    var token = localStorage.getItem("token");
+    if (token !== null) {
+      return true;
+    }
+    return false;
+  }
+}
