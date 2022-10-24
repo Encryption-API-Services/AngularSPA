d62d027a9d1a9bb5aedd5b00ba9bfaf9714cf470
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-10-24 04:51:31 -0400

    redirect to user-home page

--- src/app/modules/login/login/login.component.ts
+++ src/app/modules/login/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import LoginUserFormObject from 'src/app/models/LoginUserFormObject';
 import { HttpService } from 'src/app/services/http.service';
 import { passwordValidator } from 'src/app/validators/form-validators';
@@ -21,20 +22,29 @@ export class LoginComponent implements OnInit {
     password: ['', 
       [Validators.required, passwordValidator]]
   });
-  constructor(private formBuilder: FormBuilder, private http: HttpService) { }
+  constructor(
+    private formBuilder: FormBuilder, 
+    private http: HttpService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
 
   }
 
   public handleFormSubmit($event: any) {
-    this.isSubmitClicked = true;
-    if (this.loginForm.valid) {
+    if (this.loginForm.valid && !this.isSubmitClicked) {
+      this.isSubmitClicked = true;
       this.isFormValid = true;
-      this.http.post(this.apiUrl, this.createFormObject()).subscribe(response => {
-        console.log(response);
+      this.http.post(this.apiUrl, this.createFormObject()).subscribe((response: any) => {
+        console.log(response.token);
+        localStorage.setItem("token", response.token);
+        this.loginForm.reset();
+        this.router.navigate(['/user-home']);
+        this.isSubmitClicked = false;
       }, (error) => {
         console.error(error);
+        this.isSubmitClicked = false;
       })
     } else {
       this.isFormValid = false;
