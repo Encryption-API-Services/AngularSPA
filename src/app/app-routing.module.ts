442e0c7ade1c3a113dc125fe7ebaea393e45957e
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-10-27 17:01:22 -0400

    updating routing to point to home component if the main url is just loaded.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", loadChildren: () => import(`./modules/home/home.module`).then(m => m.HomeModule) },
   { path: "register", loadChildren: () => import(`./modules/register/register.module`).then(module => module.RegisterModule) },
   { path: "login", loadChildren: () => import(`./modules/login/login.module`).then(module => module.LoginModule) },
   { path: "about", loadChildren: () => import(`./modules/about/about.module`).then(module => module.AboutModule) },
