651cfc07ba5eb1ab16b8d1a23a5848bace8211d9
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-10-29 19:19:01 -0400

    activate component

--- src/app/modules/activate/activate/activate.component.ts
+++ src/app/modules/activate/activate/activate.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpService } from 'src/app/services/http.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-activate',
@@ -8,17 +9,23 @@ import { HttpService } from 'src/app/services/http.service';
   styleUrls: ['./activate.component.css']
 })
 export class ActivateComponent implements OnInit {
-
+  private apiUrl: string = environment.apiUrl + "UserRegister/Activate";
+  public isAuthenticating: boolean = false;
+  public wasSuccessful: boolean = false;
   constructor(private router: ActivatedRoute, private http: HttpService) { }
 
   ngOnInit(): void {
+    this.isAuthenticating = true;
     const body = {
       "id": this.router.snapshot.queryParamMap.get('id'),
       "token": this.router.snapshot.queryParamMap.get('token'),
     }
-    this.http.put("/api/UserRegister/Activate/", body).subscribe(response => {
-      console.log(response);
+    this.http.put(this.apiUrl, body).subscribe(response => {
+      this.wasSuccessful = true;
+      this.isAuthenticating = false;
     }, (error) => {
+      this.wasSuccessful = false;
+      this.isAuthenticating = false;
       console.error(error);
     });
   }
