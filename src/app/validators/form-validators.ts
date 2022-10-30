cfbbe32c087e6302af6db7c410623e75319b760f
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-10-29 22:31:23 -0400

    removing console.log

--- src/app/validators/form-validators.ts
+++ src/app/validators/form-validators.ts
@@ -6,7 +6,6 @@ import { AbstractControl } from '@angular/forms';
 export function passwordValidator(control: AbstractControl) {
     var regex: RegExp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
     if (!regex.test(control.value)) {
-        console.log("Valid")
         return { passwordValidator: {valid: false} };
     } 
     return null;
