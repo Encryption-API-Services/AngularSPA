584f5c2c1cb23ee2d1aaef0445dc72b8f597777d
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-07 20:43:39 -0500

    #22 adding toast notification for register user

--- src/app/modules/register/register/register.component.ts
+++ src/app/modules/register/register/register.component.ts
@@ -1,6 +1,7 @@
 // deno-lint-ignore-file no-inferrable-types
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import RegisterUserFormObject from 'src/app/models/RegisterUserFormObject';
 import { HttpService } from 'src/app/services/http.service';
 import { environment } from 'src/environments/environment';
@@ -25,7 +26,11 @@ export class RegisterComponent implements OnInit {
       [Validators.required, passwordValidator]]
   });
 
-  constructor(private formBuilder: FormBuilder, private http: HttpService) {
+  constructor(
+    private formBuilder: FormBuilder, 
+    private http: HttpService,
+    private toastr: ToastrService
+    ) {
 
   }
 
@@ -39,10 +44,12 @@ export class RegisterComponent implements OnInit {
       this.isSubmitClicked = true;
       this.isFormValid = true;
       this.http.post(this.apiUrl, this.createFormObject()).subscribe((response: any) => {
+        this.toastr.success("", response.message);
         this.registerForm.reset();
         this.isSubmitClicked = false;
       }, (error) => {
         console.error(error);
+        this.toastr.error("", error.error.error);
         this.isSubmitClicked = false;
       })
     } else {
