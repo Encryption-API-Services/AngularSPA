483999cabace69a090bc904457dc09e8386af767
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-09 19:32:17 -0500

    adjusting API url.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://usersapi20221024000459.azurewebsites.net/api/"
+  apiUrl: "https://encryptionusersapi.azurewebsites.net/api/"
 };
