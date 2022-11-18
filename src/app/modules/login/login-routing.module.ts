29b5f6a4570dec2a94ebd36cc2374802cc607309
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-18 07:39:21 -0500

    removing deleted componentn

--- src/app/modules/login/login-routing.module.ts
+++ src/app/modules/login/login-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TwoFactorAuthenticationComponent } from '../user-home/two-factor-authentication/two-factor-authentication.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: "", component: LoginComponent },
-  { path: "twofa", component: TwoFactorAuthenticationComponent }
+  { path: "", component: LoginComponent }
 ];
 
 @NgModule({
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -4,13 +4,11 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { TwofaComponent } from './twofa/twofa.component';
 
 
 @NgModule({
   declarations: [
-    LoginComponent,
-    TwofaComponent
+    LoginComponent
   ],
   imports: [
     CommonModule,
