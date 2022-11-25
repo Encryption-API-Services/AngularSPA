247aa5e028149eb87dec78eb1fe3f4099f58c3ce
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-25 10:33:31 -0500

    adjustments to user home layout.

--- src/app/modules/user-home/user-home-routing.module.ts
+++ src/app/modules/user-home/user-home-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuardService } from 'src/app/services/auth-guard.service';
 import { PaymentInformationComponent } from './payment-information/payment-information.component';
+import { TwoFactorAuthenticationComponent } from './two-factor-authentication/two-factor-authentication.component';
 import { UserHomeComponent } from './user-home/user-home.component';
 
 const routes: Routes = [
   {
     path: "", component: UserHomeComponent, canActivate: [AuthGuardService], children: [
+      {
+        path: "twofactor", component: TwoFactorAuthenticationComponent, canActivateChild: [AuthGuardService]
+      },
       {
         path: "payment", component: PaymentInformationComponent, canActivate: [AuthGuardService]
       }
