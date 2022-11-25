2fd6687dcffceb34a96f49dd78f46d73c2137114
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-25 10:55:24 -0500

    adding basic payment information form.

--- src/app/modules/user-home/payment-information/payment-information.component.ts
+++ src/app/modules/user-home/payment-information/payment-information.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { creditCardNumberValidator } from 'src/app/validators/form-validators';
 
 @Component({
   selector: 'app-payment-information',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment-information.component.css']
 })
 export class PaymentInformationComponent implements OnInit {
+  public paymentInfoForm: FormGroup =  this.formBuilder.group({
+    creditCardNumber: ['', [Validators.required, creditCardNumberValidator]],
+    expirationMonth: ['', [Validators.required, Validators.max(2)]],
+    expirationYear: ['', [Validators.required, Validators.max(4)]],
+    securityCode: ['', [Validators.required, Validators.max(3)]]
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+
   }
 
+  public handlePaymentInformationSubmit($event: any): void {
+    console.log(this.paymentInfoForm.valid);
+  }
 }
