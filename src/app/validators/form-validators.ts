8d24f02f591deeb65d00288f6e39e09929d7b36f
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-25 10:55:07 -0500

    adding luhn validator for the form

--- src/app/validators/form-validators.ts
+++ src/app/validators/form-validators.ts
@@ -1,20 +1,25 @@
+var luhn = require("luhn");
 import { AbstractControl } from '@angular/forms';
-
-
-
 
 export function passwordValidator(control: AbstractControl) {
     var regex: RegExp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
     if (!regex.test(control.value)) {
-        return { passwordValidator: {valid: false} };
-    } 
+        return { passwordValidator: { valid: false } };
+    }
     return null;
 }
 
 export function phoneNumberValidator(control: AbstractControl) {
     var regex: RegExp = /^((\\+91-?)|0)?[0-9]{10}$/
     if (!regex.test(control.value)) {
-        return {phoneNumberValidator: {valid: false}};
+        return { phoneNumberValidator: { valid: false } };
+    }
+    return null;
+}
+
+export function creditCardNumberValidator(control: AbstractControl) {
+    if (!luhn.validate(control.value)) {
+        return { creditCardNumberValidator: { valid: false } }
     }
     return null;
 }
