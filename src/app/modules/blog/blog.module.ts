c7aebaaa979b966ceafc319c5f416bfc4b86f3ad
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-12-07 20:43:01 -0500

    #45 create blog post text editors

--- src/app/modules/blog/blog.module.ts
+++ src/app/modules/blog/blog.module.ts
@@ -5,6 +5,8 @@ import { BlogRoutingModule } from './blog-routing.module';
 import { BlogHomeComponent } from './blog-home/blog-home.component';
 import { BlogAdminComponent } from './blog-admin/blog-admin.component';
 import { CreateBlogPostComponent } from './create-blog-post/create-blog-post.component';
+import { NgxEditorModule } from 'ngx-editor';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -15,7 +17,9 @@ import { CreateBlogPostComponent } from './create-blog-post/create-blog-post.com
   ],
   imports: [
     CommonModule,
-    BlogRoutingModule
+    BlogRoutingModule,
+    NgxEditorModule,
+    ReactiveFormsModule
   ]
 })
 export class BlogModule { }
--- src/app/modules/blog/create-blog-post/create-blog-post.component.ts
+++ src/app/modules/blog/create-blog-post/create-blog-post.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Editor, Toolbar } from 'ngx-editor';
 
 @Component({
   selector: 'app-create-blog-post',
   templateUrl: './create-blog-post.component.html',
   styleUrls: ['./create-blog-post.component.css']
 })
-export class CreateBlogPostComponent implements OnInit {
+export class CreateBlogPostComponent implements OnInit, OnDestroy {
+  public editor: Editor;
+  public toolbar: Toolbar = [
+    ['bold', 'italic'],
+    ['underline', 'strike'],
+    ['code', 'blockquote'],
+    ['ordered_list', 'bullet_list'],
+    [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
+    ['link', 'image'],
+    ['text_color', 'background_color'],
+    ['align_left', 'align_center', 'align_right', 'align_justify'],
+  ];
 
-  constructor() { }
+  public form = new FormGroup({
+    blogPostInput: new FormControl('')
+  });
+
+  constructor() {
+
+   }
+
+
 
   ngOnInit(): void {
+    this.editor = new Editor();
   }
 
+  ngOnDestroy(): void {
+    this.editor.destroy();
+  }
+
+  public handleSubmit(): void {
+    console.log(this.form.value);
+  }
 }
