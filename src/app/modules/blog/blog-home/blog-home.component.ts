465152973bbf51618cbc4a7ab13df009ee7b09e7
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-12-12 20:15:17 -0500

    #55 create basic formatting for blog posts home page

--- src/app/modules/blog/blog-home/blog-home.component.ts
+++ src/app/modules/blog/blog-home/blog-home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { HttpService } from 'src/app/services/http.service';
+import { environment } from 'src/environments/environment';
+import { BlogPost } from '../types/BlogPost';
 
 @Component({
   selector: 'app-blog-home',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog-home.component.css']
 })
 export class BlogHomeComponent implements OnInit {
+  public blogPosts: Array<BlogPost>;
 
-  constructor() { }
+  constructor(
+    private httpService: HttpService,
+    private toastr: ToastrService
+    ) { }
 
   ngOnInit(): void {
+    this.getHomeBlogPosts();
   }
 
+  public getHomeBlogPosts(): void {
+    this.httpService.get(environment.apiUrl + "Blog/GetBlogPosts").subscribe((response: any) => {
+      console.log(response);
+      this.blogPosts = response.posts;
+    });
+  }
 }
--- src/app/modules/blog/blog.module.ts
+++ src/app/modules/blog/blog.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 
 import { BlogRoutingModule } from './blog-routing.module';
 import { BlogHomeComponent } from './blog-home/blog-home.component';
@@ -7,13 +7,15 @@ import { BlogAdminComponent } from './blog-admin/blog-admin.component';
 import { CreateBlogPostComponent } from './create-blog-post/create-blog-post.component';
 import { NgxEditorModule } from 'ngx-editor';
 import { ReactiveFormsModule } from '@angular/forms';
+import { StandardDatePipe } from 'src/app/pipes/standard-date.pipe';
 
 
 @NgModule({
   declarations: [
     BlogHomeComponent,
     BlogAdminComponent,
-    CreateBlogPostComponent
+    CreateBlogPostComponent,
+    StandardDatePipe
   ],
   imports: [
     CommonModule,
