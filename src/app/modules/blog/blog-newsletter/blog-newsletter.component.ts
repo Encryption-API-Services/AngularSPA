5332f06b791b6a3f5d7ccfcdc89fe8c047b3c6dd
Mike Mulchrone <mtmulch0191@outlook.com>
2023-01-04 19:47:36 -0500

    adding newsletter subscription button to blog home page.

---
+++ src/app/modules/blog/blog-newsletter/blog-newsletter.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { HttpService } from 'src/app/services/http.service';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-blog-newsletter',
+  templateUrl: './blog-newsletter.component.html',
+  styleUrls: ['./blog-newsletter.component.css']
+})
+export class BlogNewsletterComponent implements OnInit {
+  public form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private httpService: HttpService,
+    private toastr: ToastrService
+  ) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  public handleFormSubmit(event: any): void {
+    if (this.form.valid) {
+      const body = { email: this.form.value['email'] };
+      console.log(body);
+      this.httpService.postAuthenticated(environment.apiUrl + "/Blog/Newsletter", body).subscribe((response:any ) => {
+        this.toastr.success("", response.message);
+        this.form.reset();
+      }, (error) => {
+        this.toastr.error("", error.error.error);
+      })
+    }
+  }
+}
